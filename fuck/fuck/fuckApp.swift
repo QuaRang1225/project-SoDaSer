d89db39b8d80f907b02e08d6401380589b4bf175
QuaRang1225 <dbduddnd1225@gmail.com>
2022-07-28 07:02:20 +0900

    2022/07/28

---
+++ fuck/fuck/fuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  fuckApp.swift
+//  fuck
+//
+//  Created by 유영웅 on 2022/07/26.
+//
+
+import SwiftUI
+
+@main
+struct fuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
